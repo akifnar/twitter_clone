77d5acf64c00c6b7946ba8436c77550817e0e71d
akifnar <akifnar@gmail.com>
2025-12-14 12:36:42 +0300

    Iconları ve Buttonları ekledim

---
+++ stories/Button.stories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Button from '../components/button'
+import MenuButton from '../components/menu-button'
+
+export default {
+  title: 'Buttons'
+}
+
+export const Normal = () => <Button>save</Button>
+export const Menu = () => <MenuButton>save</MenuButton>

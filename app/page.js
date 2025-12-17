2f50255ba05197e668d60b0d7bede286585e4927
akifnar <akifnar@gmail.com>
2025-12-17 18:52:47 +0300

    next page için arkaplanı düzenledim

--- app/page.js
+++ app/page.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React from 'react'
 
-import Button from "../components/button";
+import Button from '../components/button'
 
-function HomePage(){
-    return <div>
-        <h1>Welcome to Next.js</h1>
-        <Button>Merhaba</Button>
+function HomePage() {
+  return (
+    <div
+      style={{ backgroundColor: 'white', minHeight: '100vh', color: 'black' }}>
+      <h1>Welcome to Next.js</h1>
+      <Button>Merhaba</Button>
     </div>
-
+  )
 }
 
-export default HomePage;
+export default HomePage

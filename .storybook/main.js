f2f5895948026ba8674595e03f1e42958631a41d
akifnar <akifnar@gmail.com>
2025-12-17 19:49:04 +0300

    fix main.js static paths

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,20 +1,16 @@
-
-
 /** @type { import('@storybook/nextjs-vite').StorybookConfig } */
 const config = {
-  "stories": [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: [
+    '../stories/**/*.mdx',
+    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'
   ],
-  "addons": [
-    "@chromatic-com/storybook",
-    "@storybook/addon-vitest",
-    "@storybook/addon-a11y",
-    "@storybook/addon-docs"
+  addons: [
+    '@chromatic-com/storybook',
+    '@storybook/addon-vitest',
+    '@storybook/addon-a11y',
+    '@storybook/addon-docs'
   ],
-  "framework": "@storybook/nextjs-vite",
-  "staticDirs": [
-    "..\\public"
-  ]
-};
-export default config;
+  framework: '@storybook/nextjs-vite',
+  staticDirs: ['../public']
+}
+export default config
